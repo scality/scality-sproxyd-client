66041b3d4f92de4421d97b67d199c26d876a5a2d
jgagnaire <jean.gagnaire@scality.com>
2018-10-17 16:18:07 +0200

    Removed Python 2.6 from supported versions

--- setup.py
+++ setup.py
@@ -73,7 +73,6 @@ setuptools.setup(
     classifiers=[
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7'],
     install_requires=scality_sproxyd_client.__requires__,
 )
